0c0c9d09d7e0bb65641acc0756b4dc2187bc0464
Viacheslaw Saveliew <version-1@yandex.ru>
2023-11-27 01:38:29 +0300

    map

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-import springs from './db/springs.json'
+import data from './db/springs.json'
+
+let springs = data.data
 
 ymaps.ready(function () {
 
     var myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
-        zoom: 10
+        zoom: 8,
+        controls: ['zoomControl']
     });
+
+    // Внешний вид карты
+    //myMap.setType('yandex#satellite');
+
+    //Убрать лишние элементы
+    $('[class*=copyright]').hide()
+
+    //коллекция
+    var myPlacemark = {}
+    for(let i=0; i < springs.length; i++) {
+
+        console.log( '***', springs[i] );
+
+        var myPlacemark = new ymaps.Placemark(
+            springs[i].coords,
+            {
+                balloonContent: springs[i].balloonContent,
+                iconCaption: springs[i].name
+            },
+            {
+                preset: 'twirl#redIcon'
+            });
+
+        myMap.geoObjects.add(myPlacemark);
+    }
+
 });
